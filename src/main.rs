cdac601906749d80692880cf7eab90ca435ed78d
Pekka Raisio <raisiope@gmail.com>
2026-04-14 04:15:07 +0000

    Updated

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,7 @@ use serde_json::json;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0").await?;
-    // Kokeile porttia 6791, joka on MeshCom-standardi
     let target_addr = "192.168.68.144:1799"; 
-
-    // let payload = json!({
-    //     "type": "msg",
-    //     "src": "OH6CXK-10",
-    //     "dst": "ALL",
-    //     "msg": "K B4 A"
-    // });
-
-    // let payload = json!({
-    //     "type": "msg",
-    //     "src_type": "node",  // Lisätty: kertoo että lähettäjä on solmu
-    //     "src": "OH6CXK-10",
-    //     "dst": "ALL",
-    //     "msg": "K B4 A"
-    // });
-
-    // let payload = json!({
-    //     "src_type": "lora",  // Kokeile huijata: väitä että viesti tuli radiolta
-    //     "type": "msg",
-    //     "src": "OH6CXK-10",
-    //     "dst": "ALL",
-    //     "msg": "K B4 A",
-    //     "msg_id": "B001E0FF" // Lisätty kuvitteellinen ID
-    // });
 
     let payload = json!({
         // "src_type": "node", // Kerrotaan, että viesti tulee solmulta
